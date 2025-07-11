edb6cd67235768d2604cad1de8232046e7b66c5f
SOHAM3705 <sohampachpute05@gmail.com>
2025-07-11 18:19:56 +0530

    tailwind css added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import AddUser from "./components/AddUser";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-center text-2xl my-4">User Management</h1>
-      <AddUser />
     </div>
   );
 }
