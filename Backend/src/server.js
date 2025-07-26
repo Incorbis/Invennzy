6e68867300860c91654f579a384d80b2a132d82c
aniruddha9674 <aniruddhanalawade1234@gmail.com>
2025-07-26 22:22:40 +0530

    connection to mysql

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors'); 
+require('./models/db'); 
 const contactusRoute = require('./routes/contactusRoute');
+
 const app = express();
 
 app.use(cors());
@@ -9,6 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', contactusRoute);
+
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');});
