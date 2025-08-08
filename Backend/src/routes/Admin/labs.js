ecc1796808af7007333b8149784b14f7ad952c2f
SOHAM3705 <sohampachpute05@gmail.com>
2025-08-08 19:00:43 +0530

    Server started

--- Backend/src/routes/Admin/labs.js
+++ Backend/src/routes/Admin/labs.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Lab = require('../models/Labs');
-const db = require('../db'); // Add DB connection here
+const db = require('../../db'); // Add DB connection here
 const Lab = require('../../models/Labs');
 
 // ✅ GET labs by adminId
